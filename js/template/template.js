b77f8f36dc84e4230a91dfc80ca59c7374bb97db
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-12 10:18:52 +0700

    zzz

--- js/template/template.js
+++ js/template/template.js
@@ -1,4 +1,4 @@
-export let URLGeoJson = "https://us-central1-vivid-vent-401501.cloudfunctions.net/function-2";
+export let URLGeoJson = "https://us-central1-annular-hexagon-401501.cloudfunctions.net/function-3";
 export let urlPostGCF = "https://asia-southeast2-annular-hexagon-401501.cloudfunctions.net/postgisfancyfixxxx";
 export let tableTag="tr";
 export let tableRowClass="content is-small";
